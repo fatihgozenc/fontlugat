2db1da25ef6e5256357f1655b1f417eec5e915ae
Fatih Gozenc <fatihgozenc@gmail.com>
2020-10-15 22:06:07 +0200

    errors handled, external path usage implemented

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 mod fonts;
+use std::io;
 
 fn main() {
-	let fonts = fonts::get("./fonts");
-	let generate = fonts::generate(fonts);
-	match generate {
-			Ok(v) => v,
-			Err(e) => println!("HTML error! : {:?}", e),
-	}
-	open::that("./fonts.html");
+    println!("Enter font folder path: ");
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input){
+        Ok(_n) => {
+            let fonts = fonts::get(&input.to_string().trim_end());
+            match fonts::generate(fonts){
+                Ok(v) => v,
+                Err(e) => println!("HTML error! : {:?}", e),
+            }
+            match open::that("./fonts.html"){
+                Ok(v) => println!("{:?}. Look at your browser.", v),
+                Err(e) => println!("{:?}", e)
+            }
+        }
+        Err(error) => println!("{:?} has err: {:?}", input, error)
+    }
 }
