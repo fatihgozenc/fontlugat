d8a08930a32a8ed1eaf9fe7c813d66c01547bb48
Fatih Gozenc <fatihgozenc@gmail.com>
2021-04-10 15:23:11 +0200

    new trials

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,6 @@ mod gui;
 mod lib;
 
 fn main() {
-    // lib::run("./fonts");
+    lib::run("./fonts");
     gui::init("./src/templates/test.html");
 }
-
-// // let fonts = fonts::get(&dir);
-// // println!("{:?}", fonts);
-// // match fonts::generate_css(fonts){
-// //     Ok(res) => res,
-// //     Err(err) => println!("HTML compilation error!: {:?}", err),
-// // }
-// // match open::that(if cfg!(windows){".\\fontlugat.html"} else {"./fontlugat.html"}){
-// //     Ok(_res) => println!("Successfully initialized!"),
-// //     Err(err) => println!("Browser error!: {:?}", err)
-// // }
